17e8eda27c4a0c9a79af765e059b2061711d14b7
weisscon <145080350+weisscon@users.noreply.github.com>
2024-11-21 12:21:17 -0500

    password reset functionality
    users can reset functionality

--- chorister_db/general.py
+++ chorister_db/general.py
@@ -3,6 +3,8 @@ from flask import (
 )
 from werkzeug.exceptions import abort
 
+from werkzeug.security import check_password_hash, generate_password_hash
+
 from chorister_db.auth import login_required
 from chorister_db.db import get_db
 from chorister_db.db import get_users
@@ -22,3 +24,43 @@ def index():
     for row in raw_permissions:
         permissions.append(row['permissionId'])
     return render_template('general/index.html', permissions = permissions)
+
+@bp.route('/password-reset', methods=('GET', 'POST'))
+@login_required
+def reset_password():
+    if request.method == 'POST':
+        password = request.form['password']
+        new_password = request.form['new_password']
+        confirm_password = request.form['pass_confirm']
+        error = None
+
+        users = get_users()
+        user = users.execute(
+            'SELECT * FROM account WHERE accountId = ?',(g.user['accountId'],)
+        ).fetchone()
+
+        if not check_password_hash(user['password'], password):
+            error = 'Previous password entered incorrectly'
+        if new_password != confirm_password:
+            error = 'New Password does not match confirmation'
+        
+        if error == None:
+            try:
+                users.execute(
+                    'UPDATE account SET password = ? WHERE accountId = ?',
+                    (generate_password_hash(new_password), g.user['accountId'],)
+                )
+                print('update completed')
+                users.commit()
+                flash('Password updated')
+            except Exception as err:
+                flash(err)
+        else: 
+            flash(error)
+    
+    return render_template('general/passwordreset.html')
+
+
+
+
+
